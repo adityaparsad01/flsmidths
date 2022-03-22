f176642177a9c1f35923dd7aa29c8cdea8ff40dd
aditya <adityaparsad01@gmail.com>
2022-03-22 17:15:31 +0530

    animation

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import Employee from "./employess";
 export default function App() {
   const time = new Date();
   return (
-    <div className="App">
-      <div className="header">
-        <h1>Employees Vehicle Data</h1>
-        <h2>{time.toDateString()}</h2>
+    <>
+      <div className="container">
+        <div className="bg"></div>
+        <div className="bg bg2"></div>
+        <div className="bg bg3"></div>
       </div>
-      <Employee />
-    </div>
+      <div className="App">
+        <div className="header">
+          <h1>Employees Vehicle Data</h1>
+          <h2>{time.toDateString()}</h2>
+        </div>
+        <Employee />
+      </div>
+    </>
   );
 }
