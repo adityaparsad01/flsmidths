8705487ecf795db2ccfe4d6541870788b7d2df61
aditya <adityaparsad01@gmail.com>
2022-03-26 06:49:38 +0530

    PUSH

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import Employee from "./employess";
 
 export default function App() {
+  const setSearch = () => {};
   const time = new Date();
   return (
     <>
@@ -10,12 +11,22 @@ export default function App() {
         <div className="bg bg2"></div>
         <div className="bg bg3"></div>
       </div>
-      <div className="header">
+      <div className="naev">
+        <div className="header">
           <h1>Employees Vehicle Data</h1>
           <h2>{time.toDateString()}</h2>
         </div>
+        <div>
+          <input
+            className="search"
+            type="text"
+            placeholder="Search Employees"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
+      </div>
       <div className="App">
-        <Employee className="emp"/>
+        <Employee className="emp" />
       </div>
     </>
   );
--- src/employess.jsx
+++ src/employess.jsx
@@ -2,29 +2,40 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { BsFillPersonFill } from "react-icons/bs";
 import { AiFillCar } from "react-icons/ai";
-import { IoTodaySharp } from "react-icons/io";
 
 const URL =
   "https://script.googleusercontent.com/macros/echo?user_content_key=Py9gIiBc4YOM_ITm5aYCR948nNXeoaivGVHnxL1EIqvxTBiEp5PIx36hyRezgNwVhAVGXgIQCiBuhVIuvluQ9doGl-NNH8lhm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDjnOsdLBxrqKlg66fXIurI5lavxEO2QCYwQ_uURYMbDIWlFMj978FfjMzJRHa9fX5XLva7GBrCe4j-XCzu0DKPb3lj62okUAQ&lib=MhYwMEYR0SkuKYxjEuQPjnR2TY0KEY2cc";
 
 const Employee = () => {
   const [emp, setEmp] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const newEmp = emp.sort();
   console.log(emp);
 
   useEffect(() => {
+    setLoading(true);
     axios
       .get(URL)
       .then(function (response) {
         setEmp(response.data[0].jsonArray);
+        setLoading(false);
       })
       .catch(function (error) {
         console.log(error);
       });
   }, []);
 
+  if (loading) {
+    return (
+      <div class="center-div">
+        <img src="https://tenor.com/bkN6Z.gif" alt="" srcset="" />
+      </div>
+    );
+  }
+
   return (
     <>
-      {emp.map((item, idx) => {
+      {newEmp.map((item, idx) => {
         return (
           <div className="card" key={idx}>
             <h1 className="name">
