f9e99e8bc035bc9cbb51f67c74240cbc14c69f93
aditya <adityaparsad01@gmail.com>
2022-03-26 08:28:50 +0530

    change

--- src/App.js
+++ src/App.js
@@ -13,22 +13,19 @@ export default function App() {
   const time = new Date();
   return (
     <>
-      <div className="container">
-        <div className="bg"></div>
-        <div className="bg bg2"></div>
-        <div className="bg bg3"></div>
-      </div>
-      <div className="nav">
-        <div className="header">
-          <h1>Vehicle Data</h1>
-          <h2>{time.toDateString()}</h2>
+      <div className="main">
+        <div className="nav">
+          <div className="header">
+            <h1>Vehicle Data</h1>
+            <h2>{time.toDateString()}</h2>
+          </div>
+          <div>
+            <Search inputval={inputval} />
+          </div>
         </div>
-        <div>
-          <Search inputval={inputval} />
+        <div className="App">
+          <Employee className="emp" search={search} />
         </div>
-      </div>
-      <div className="App">
-        <Employee className="emp" search={search} />
       </div>
     </>
   );
